299d8487a75b6942daa66adff5f47d1cc2a70266
Yusuf Emritte <34180742+ysfemritte@users.noreply.github.com>
2021-09-17 17:45:40 +0400

    Add files via upload
    An application that simulates the tossing of a coin. Each time
    the user tosses the coin, the application uses a Random object to get a random integer in
    the range of 0 through 1. If the random number is 0, it means the tails side of the coin
    is up, and if the random number is 1, it means the heads side is up. The application
    displays an image of a coin showing either heads or tails, depending on the value of the
    random number.

---
+++ Coin Toss/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Coin_Toss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.headsPictureBox = new System.Windows.Forms.PictureBox();
+            this.tailsPictureBox = new System.Windows.Forms.PictureBox();
+            this.tossButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // headsPictureBox
+            // 
+            this.headsPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("headsPictureBox.Image")));
+            this.headsPictureBox.Location = new System.Drawing.Point(22, 38);
+            this.headsPictureBox.Name = "headsPictureBox";
+            this.headsPictureBox.Size = new System.Drawing.Size(188, 188);
+            this.headsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.headsPictureBox.TabIndex = 0;
+            this.headsPictureBox.TabStop = false;
+            // 
+            // tailsPictureBox
+            // 
+            this.tailsPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("tailsPictureBox.Image")));
+            this.tailsPictureBox.Location = new System.Drawing.Point(22, 38);
+            this.tailsPictureBox.Name = "tailsPictureBox";
+            this.tailsPictureBox.Size = new System.Drawing.Size(188, 188);
+            this.tailsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tailsPictureBox.TabIndex = 0;
+            this.tailsPictureBox.TabStop = false;
+            // 
+            // tossButton
+            // 
+            this.tossButton.Location = new System.Drawing.Point(12, 241);
+            this.tossButton.Name = "tossButton";
+            this.tossButton.Size = new System.Drawing.Size(94, 36);
+            this.tossButton.TabIndex = 1;
+            this.tossButton.Text = "Toss";
+            this.tossButton.UseVisualStyleBackColor = true;
+            this.tossButton.Click += new System.EventHandler(this.tossButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(128, 241);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(94, 36);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 298);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.tossButton);
+            this.Controls.Add(this.tailsPictureBox);
+            this.Controls.Add(this.headsPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox headsPictureBox;
+        private System.Windows.Forms.PictureBox tailsPictureBox;
+        private System.Windows.Forms.Button tossButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Coin Toss/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coin_Toss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tossButton_Click(object sender, EventArgs e)
+        {
+            // Variable to indicate which side is up 
+            int sideUp;
+
+            //Create random object
+            Random rand = new Random();
+
+            // Get a random integer in the range of 0 through 1.
+            // 0 means tails up, 1 means heads up.
+            sideUp = rand.Next(2);
+
+            // Display the side that is up.
+            if (sideUp == 0)
+            {
+                // Display tails up.
+                tailsPictureBox.Visible = true;
+                headsPictureBox.Visible = false;
+            }
+            else
+            {
+                // Display heads up.
+                headsPictureBox.Visible = true;
+                tailsPictureBox.Visible = false;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
